1e0fed0e684e75623b54cf2874a0fbb66de24fbd
Sanghee Kim <sangheek16@gmail.com>
2022-09-22 20:40:34 -0500

    update on README and evaluate.py

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
 # About
 This repo contains datasets and code for COLING 2022 paper _“No, they did not”: Dialogue response dynamics in pre-trained language
-models_, by Sanghee Kim, Lang Yu and Allyson Ettinger.
+models_, by Sanghee J. Kim, Lang Yu and Allyson Ettinger.
 
 # Repo Structures
 
@@ -8,15 +8,15 @@ models_, by Sanghee Kim, Lang Yu and Allyson Ettinger.
 - `datasets/` contains raw data and datasets used in the paper
 
 # Dataset
-- `names.csv`, `np.csv` and `vp.csv` are raw datasets we used to generate analysis datasets. They can be used as the input to `input_generator.py` to generate files in the same format as `used_items.csv`.
-- `used_items.csv` are items we used in the paper.
+- `names.csv`, `np.csv` and `vp.csv` are raw datasets that can be used as inputs for `input_generator.py` to generate an input .csv file -- this will have the same format as `used_items.csv`
+- `used_items.csv` contains the items that were used in this paper
 
 # Code
-- code is built on Huggingface's [transformer](https://github.com/huggingface/transformers)
-- evaluation requires [minicons](https://github.com/kanishkamisra/minicons)
-- `input_generator.py` randomly samples from names.csv, np.csv and vp.csv to generate analysis dataset we used in the paper. To replicate the result with exact input, use "used_items.csv" included in the folder instead.
-- `evaluate.py` contains analysis for header selection (section 5.1), rejection (section 5.2), conjunction (section 5.3) and ellipsis (section 6).
+- `evaluate.py` is built on Huggingface's [transformer](https://github.com/huggingface/transformers) and requires [minicons](https://github.com/kanishkamisra/minicons)
+- `input_generator.py` randomly samples from `names.csv`, `np.csv` and `vp.csv` and generates input items that can be used for model evaluation
+- for replication of the results in this paper, use `used_items.csv` under `src/` instead of generating new items
+- `evaluate.py` produces model output probabilities / conditional log-probabilities / pseudo-log-likelihoods of causal language model and masked language models. It produces output for *header selection* (section 5.1), *rejection* (section 5.2), *conjunction* (section 5.3) and *ellipsis* (section 6) tasks.
 - `probe.py` contains code for generating probing results (section 5.4)
 
 # Usage
-To run, update config part at the begining of the file (model, model type, input output path etc.), then run `python3 file_to_run.py`.
+To run `evaluate.py`, update config at the beginning of the code (e.g., input path, output path, etc.). Also update the name of model (`model_name` variable) and type of task (`task` variable) you wish to explore, at the end of the code. Then run `python3 evaluate.py`.
--- src/evaluate.py
+++ src/evaluate.py
@@ -1,8 +1,4 @@
-import itertools
-import random
-import sys
 import pandas as pd
-import numpy as np
 import os
 from timeit import default_timer as timer
 import datetime
@@ -11,26 +7,19 @@ from transformers import AutoModelForMaskedLM
 from transformers import AutoTokenizer
 import torch
 
-# header selection file 
+
 # --- config --- #
 INPUT_PAIRS = '../datasets/used_items.csv'
 OUTPUT_DIR = '../out'
-model_name= "roberta-base" # supported models ['distilbert-base-cased', 'distilroberta-base', 'xlm-roberta-base', 'distilgpt2', 'roberta-base', 'bert-base-cased', 'gpt2']
-task = 'header_selection' # available tasks: [header_selection, rejection, conjunction, ellipsis]
 
-
+# create path
 if not os.path.exists(OUTPUT_DIR):
     os.makedirs(OUTPUT_DIR)
 
+# supported models and tasks
+_supported_models = ['bert-base-cased', 'roberta-base', 'xlm-roberta-base', 'distilbert-base-cased', 'distilroberta-base', 'distilgpt2']
+_supported_tasks = ['header_selection', 'rejection', 'conjunction', 'ellipsis']
 
-if model_name == 'distilgpt2':
-    model_type = 'CLM'
-else:
-    model_type = 'MLM'
-
-_start_ind = 0
-input_size = 600 #1200 # length of the input dataframe
-chunk_size = input_size - _start_ind #<-- needs to be in multiples of 100
 
 '''
 --------------------------------------------------------------------
@@ -38,16 +27,12 @@ chunk_size = input_size - _start_ind #<-- needs to be in multiples of 100
 --------------------------------------------------------------------
 '''
 
-# cleaning input material
-df = pd.read_csv(INPUT_PAIRS)
-df1, df2 = df.copy(deep=True), df.copy(deep=True)
-df1['header'], df2['header'] = 'reject', 'wait'
-df = pd.concat([df1, df2])
-df.reset_index(inplace=True, drop=True)
-
-# dictionary for header
-header = {}
-header['reject'] = 'No'; header['wait'] = 'Wait no'
+# including headers in the input
+df_input = pd.read_csv(INPUT_PAIRS)
+df_input1, df_input2 = df_input.copy(deep=True), df_input.copy(deep=True)
+df_input1['header'], df_input2['header'] = 'reject', 'wait'
+df_input = pd.concat([df_input1, df_input2])
+df_input.reset_index(inplace=True, drop=True)
 
 
 '''
@@ -56,8 +41,19 @@ header['reject'] = 'No'; header['wait'] = 'Wait no'
 --------------------------------------------------------------------
 '''
 
+# function for getting prediction with clm
+def scorer_clm(x, clm_model):
+    start_time = datetime.datetime.now()
+    score_raw = clm_model.sequence_score(x)[0] # log probability (/ surprisal) normalized by number of tokens
+    end_time = datetime.datetime.now()
+    execution_time = (end_time - start_time).total_seconds() * 1
+    print('It took %d seconds to process this row' %(execution_time))
+    return score_raw
+
+# function for getting prediction with mlm
+# .. for rejection & conjunction tasks
 def fill_mask_target(model_name, seq, target_words, tokenizer, lang_model):
-    # target_words should be in list
+    # target_words should be in a list (e.g., ['did'], ['did', 'does'])
 
     sequence = seq.replace('[MASK]', tokenizer.mask_token)
     inputs = tokenizer(sequence, return_tensors="pt")
@@ -76,7 +72,31 @@ def fill_mask_target(model_name, seq, target_words, tokenizer, lang_model):
     target_probs = dict()
     for k,v in target_ids.items():
             target_probs[k] = round(probs[..., v].item(),4)
-    return target_probs
+    return target_probs # different output from fill_verb_target
+
+# function for getting prediction with mlm
+# .. for ellipsis task
+def fill_verb_target(model_name, seq, target_words, tokenizer, lang_model):
+    # target_words should be in list
+
+    sequence = seq.replace('[VERB]', tokenizer.mask_token)
+    inputs = tokenizer(sequence, return_tensors="pt")
+    mask_token_index = torch.where(inputs["input_ids"] == tokenizer.mask_token_id)[1]
+    token_logits = lang_model(**inputs).logits 
+    mask_token_logits = token_logits[0, mask_token_index, :]
+    probs = mask_token_logits.softmax(dim = 1)
+    
+    target_ids = dict()
+    for word in target_words:
+        if model_name == "distilroberta-base":
+                target_ids[word] = tokenizer.encode(str(' ') + word)[1]
+        else:
+                target_ids[word] = tokenizer.encode(word)[1]
+
+    target_probs = dict()
+    for k,v in target_ids.items():
+            target_probs[k] = round(probs[..., v].item(),4)
+    return list(target_probs.items()) # different output from fill_mask_target
 
 '''
 --------------------------------------------------------------------
@@ -85,29 +105,25 @@ def fill_mask_target(model_name, seq, target_words, tokenizer, lang_model):
 '''
 
 def header_selection(df, model_type):
+    # dictionary for header
+    header = {}
+    header['reject'] = 'No'; header['wait'] = 'Wait no'
+
     # create input sequence
     df['seq1'] = df.apply(lambda x: '%s said, "%s, who %s, %s," and %s replied, "%s, %s %s not."' %(x['name1'], x['subj'], x['vp1'], x['vp2'], x['name2'], header[x['header']], x['prn'], x['verb1']), axis=1)
     df['seq2'] = df.apply(lambda x: '%s said, "%s, who %s, %s," and %s replied, "%s, %s %s not."' %(x['name1'], x['subj'], x['vp1'], x['vp2'], x['name2'], header[x['header']], x['prn'], x['verb2']), axis=1)
-    if model_type == 'CLM':
+    
+    # get predictions
+    print('** Getting predictions ... **')
 
+    if model_type == 'CLM':
         clm_model = scorer.IncrementalLMScorer('distilgpt2', 'cpu')
 
-        def scorer_clm(x):
-            start_time = datetime.datetime.now()
-            score_raw = clm_model.sequence_score(x)[0] # log probability (/ surprisal) normalized by number of tokens
-            end_time = datetime.datetime.now()
-            execution_time = (end_time - start_time).total_seconds() * 1
-            print('It took %d seconds to process this row' %(execution_time))
-            return score_raw
+        df['model'] = model_name
+        df['score1'] = df['seq1'].map(lambda x: scorer_clm(x, clm_model))
+        df['score2'] = df['seq2'].map(lambda x: scorer_clm(x, clm_model))
 
-        data_clm = df.copy(deep=True)
-        data_clm['model_name'] = 'distilgpt2'
-        data_clm['score1'] = data_clm['seq1'].map(lambda x: scorer_clm(x))
-        data_clm['score2'] = data_clm['seq2'].map(lambda x: scorer_clm(x))
-
-        data_clm.to_csv('%s/%s_header_selection.csv' %(OUTPUT_DIR, model_name), index=False)
     elif model_type == 'MLM':
-
         if model_name == 'distilbert-base-cased':
             print('> .. Importing distilbert-base-cased')
             mlm_model = scorer.MaskedLMScorer('distilbert-base-cased', 'cpu')
@@ -132,29 +148,47 @@ def header_selection(df, model_type):
             print('It took %d seconds to process this row' %(execution_time))
             return score_raw
 
-        data_mlm = df.copy(deep=True)
-        data_mlm['model_name'] = model_name
-        data_mlm['score1'] = data_mlm['seq1'].map(lambda x: scorer_mlm(x))
-        data_mlm['score2'] = data_mlm['seq2'].map(lambda x: scorer_mlm(x))
+        df['model'] = model_name
+        df['score1'] = df['seq1'].map(lambda x: scorer_mlm(x))
+        df['score2'] = df['seq2'].map(lambda x: scorer_mlm(x))
 
-        data_mlm.to_csv('%s/%s_header_selection.csv' %(OUTPUT_DIR,  model_name), index=False)
     else:
-        print("unsupported model type {}".format(model_type))
+        raise ValueError("Unsupported model type: {}".format(model_type))
+
+    _output_folder = '%s/%s/' %(OUTPUT_DIR, model_name)
+    if not os.path.exists(_output_folder):
+        os.makedirs(_output_folder)
+    df.to_csv(_output_folder + 'header_selection-%s.csv' %(model_name), index=False)
+    # return data_mlm
 
 
-def rejection(df):
+def rejection(df, model_type):
+    # dictionary for header
+    header = {}
+    header['reject'] = 'No'; header['wait'] = 'Wait no'
+
     # create input sequence
     df['seq1'] = df.apply(lambda x: '%s said, "%s, who %s, %s," and %s replied, "%s, %s %s not."' %(x['name1'], x['subj'], x['vp1'], x['vp2'], x['name2'], header[x['header']], x['prn'], x['verb1']), axis=1)
     df['seq2'] = df.apply(lambda x: '%s said, "%s, who %s, %s," and %s replied, "%s, %s %s not."' %(x['name1'], x['subj'], x['vp1'], x['vp2'], x['name2'], header[x['header']], x['prn'], x['verb2']), axis=1)
 
-    tokenizer = AutoTokenizer.from_pretrained(model_name)
-    lang_model = AutoModelForMaskedLM.from_pretrained(model_name)
+    # initialize language models
+    if model_type == 'CLM':
+        clm_model = scorer.IncrementalLMScorer(model_name, 'cpu')
+
+    elif model_type == 'MLM':
+        tokenizer = AutoTokenizer.from_pretrained(model_name)
+        lang_model = AutoModelForMaskedLM.from_pretrained(model_name)
+
+    else:
+        raise ValueError("Unsupported model type: {}".format(model_type))
+
+    # get predictions
+    print('** Getting predictions ... **')
 
     prediction_dict = {}
     row_count = 0
     total_time = 0
 
-    print('** Getting predictions ... **')
     for idn, row in df.iloc[:_start_ind + chunk_size].iterrows():
         if idn < _start_ind:
             row_count += 1
@@ -180,17 +214,24 @@ def rejection(df):
         prediction_dict[row_count]['vp2'] = vp2
         prediction_dict[row_count]['model'] = model_name
 
-        seq1 = seq1.replace(verb1 + ' not.', '[MASK] not.')
-        seq2 = seq2.replace(verb2 + ' not.', '[MASK] not.')
-        prediction_dict[row_count]['seq1'] = seq1
-        prediction_dict[row_count]['seq2'] = seq2
-        prediction_dict[row_count]['score1'] = fill_mask_target(model_name, seq1, [verb1], tokenizer, lang_model)[verb1]
-        prediction_dict[row_count]['score2'] = fill_mask_target(model_name, seq2, [verb2], tokenizer, lang_model)[verb2]
+        if model_type == 'CLM':
+            prediction_dict[row_count]['seq1'] = seq1
+            prediction_dict[row_count]['seq2'] = seq2
+            prediction_dict[row_count]['score1'] = scorer_clm(seq1, clm_model)
+            prediction_dict[row_count]['score2'] = scorer_clm(seq2, clm_model)     
+
+        elif model_type == 'MLM':
+            prediction_dict[row_count]['seq1'] = seq1
+            prediction_dict[row_count]['seq2'] = seq2
+            seq1 = seq1.replace(verb1 + ' not.', '[MASK] not.')
+            seq2 = seq2.replace(verb2 + ' not.', '[MASK] not.')
+            prediction_dict[row_count]['score1'] = fill_mask_target(model_name, seq1, [verb1], tokenizer, lang_model)[verb1]
+            prediction_dict[row_count]['score2'] = fill_mask_target(model_name, seq2, [verb2], tokenizer, lang_model)[verb2]
 
         end = timer()
         row_count += 1
 
-        print('%d/%d' %(row_count, chunk_size)) # print('%d/%d' %(row_count, len(df)*len(_models_clm+_models_mlm)))
+        print('%d/%d' %(row_count, chunk_size))
         print('elapsed time for one iteration: %d' %(end-start))
         total_time += end-start
         print('total elapsed time: %d' %(total_time))
@@ -200,21 +241,36 @@ def rejection(df):
             if not os.path.exists(_output_folder):
                 os.makedirs(_output_folder)
             pd.DataFrame(prediction_dict).T.to_csv(_output_folder + 'rejection-evaluation-%s_row%d.csv' %(model_name, row_count), index=False)
+    # return prediction_dict
 
 
-def conjunction(df):
+def conjunction(df, model_type):
+    # dictionary for header
+    header = {}
+    header['reject'] = 'No'; header['wait'] = 'Wait no'
+
     # create input sequence
     df['seq1'] = df.apply(lambda x: '%s said, "%s %s and %s," and %s replied, "%s, %s %s not."' %(x['name1'], x['subj'], x['vp1'], x['vp2'], x['name2'], header[x['header']], x['prn'], x['verb1']), axis=1)
     df['seq2'] = df.apply(lambda x: '%s said, "%s %s and %s," and %s replied, "%s, %s %s not."' %(x['name1'], x['subj'], x['vp1'], x['vp2'], x['name2'], header[x['header']], x['prn'], x['verb2']), axis=1)
 
-    tokenizer = AutoTokenizer.from_pretrained(model_name)
-    lang_model = AutoModelForMaskedLM.from_pretrained(model_name)
+    # initialize language models
+    if model_type == 'CLM':
+        clm_model = scorer.IncrementalLMScorer(model_name, 'cpu')
+
+    elif model_type == 'MLM':
+        tokenizer = AutoTokenizer.from_pretrained(model_name)
+        lang_model = AutoModelForMaskedLM.from_pretrained(model_name)
+
+    else:
+        raise ValueError("Unsupported model type: {}".format(model_type))
+
+    # get predictions
+    print('** Getting predictions ... **')
 
     prediction_dict = {}
     row_count = 0
     total_time = 0
 
-    print('** Getting predictions ... **')
     for idn, row in df.iloc[:_start_ind + chunk_size].iterrows():
         if idn < _start_ind:
             row_count += 1
@@ -240,17 +296,24 @@ def conjunction(df):
         prediction_dict[row_count]['vp2'] = vp2
         prediction_dict[row_count]['model'] = model_name
 
-        seq1 = seq1.replace(verb1 + ' not.', '[MASK] not.')
-        seq2 = seq2.replace(verb2 + ' not.', '[MASK] not.')
-        prediction_dict[row_count]['seq1'] = seq1
-        prediction_dict[row_count]['seq2'] = seq2
-        prediction_dict[row_count]['value1'] = fill_mask_target(model_name, seq1, [verb1], tokenizer, lang_model)[verb1]
-        prediction_dict[row_count]['value2'] = fill_mask_target(model_name, seq2, [verb2], tokenizer, lang_model)[verb2]
+        if model_type == 'CLM':
+            prediction_dict[row_count]['seq1'] = seq1
+            prediction_dict[row_count]['seq2'] = seq2
+            prediction_dict[row_count]['score1'] = scorer_clm(seq1, clm_model)
+            prediction_dict[row_count]['score2'] = scorer_clm(seq2, clm_model)
 
+        elif model_type == 'MLM':
+            prediction_dict[row_count]['seq1'] = seq1
+            prediction_dict[row_count]['seq2'] = seq2
+            seq1 = seq1.replace(verb1 + ' not.', '[MASK] not.')
+            seq2 = seq2.replace(verb2 + ' not.', '[MASK] not.')
+            prediction_dict[row_count]['score1'] = fill_mask_target(model_name, seq1, [verb1], tokenizer, lang_model)[verb1]
+            prediction_dict[row_count]['score2'] = fill_mask_target(model_name, seq2, [verb2], tokenizer, lang_model)[verb2]
+            
         end = timer()
         row_count += 1
 
-        print('%d/%d' %(row_count, chunk_size)) # print('%d/%d' %(row_count, len(df)*len(_models_clm+_models_mlm)))
+        print('%d/%d' %(row_count, chunk_size))
         print('elapsed time for one iteration: %d' %(end-start))
         total_time += end-start
         print('total elapsed time: %d' %(total_time))
@@ -261,22 +324,37 @@ def conjunction(df):
                 os.makedirs(_output_folder)
             pd.DataFrame(prediction_dict).T.to_csv(_output_folder + 'conjunction-evaluation-%s_row%d.csv' %(model_name, row_count), index=False)
 
+    # return prediction_dict
 
-def ellipsis(df):
+
+def ellipsis(df, model_type):
+    # dictionary for header
+    header = {}
+    header['reject'] = 'No'; header['wait'] = 'Wait no'
+
     # create input sequence
     df['seq'] = df.apply(lambda x: '%s said, "%s, who %s, %s," and %s replied, "%s, %s [VERB] not."' %(x['name1'], x['subj'], x['vp1'], x['vp2'], x['name2'], header[x['header']], x['prn']), axis=1)
 
-    verb_list = list(df.verb1.unique())
+    verb_list = list(df.verb1.unique()) # verb_list = list(df.verb2.uniuqe())
 
-    tokenizer = AutoTokenizer.from_pretrained(model_name)
-    lang_model = AutoModelForMaskedLM.from_pretrained(model_name)
+    # initialize language models
+    if model_type == 'CLM':
+        clm_model = scorer.IncrementalLMScorer(model_name, 'cpu')
+
+    elif model_type == 'MLM':
+        tokenizer = AutoTokenizer.from_pretrained(model_name)
+        lang_model = AutoModelForMaskedLM.from_pretrained(model_name)
+
+    else:
+        raise ValueError("Unsupported model type: {}".format(model_type))
+
+    # get predictions
+    print('** Getting predictions ... **')
 
     prediction_dict = {}
     row_count = 0
     total_time = 0
 
-
-    print('** Getting predictions ... **')
     for idn, row in df.iloc[:_start_ind + chunk_size].iterrows():
 
         if idn < _start_ind:
@@ -300,14 +378,22 @@ def ellipsis(df):
         prediction_dict[row_count]['verb2'] = verb2
         prediction_dict[row_count]['vp1'] = vp1
         prediction_dict[row_count]['vp2'] = vp2
-
         prediction_dict[row_count]['seq'] = seq
-        prediction_dict[row_count]['vs_pair'] = fill_mask_target(model_name, seq, verb_list, tokenizer, lang_model)
+
+        if model_type == 'CLM':
+            prediction_dict[row_count]['vs_pair'] = []
+            for verb in verb_list:
+                seq_replaced = seq.replace('[VERB]', verb)
+                verb_score = (verb, scorer_clm(seq_replaced, clm_model))
+                prediction_dict[row_count]['vs_pair'].append(verb_score)
+        
+        elif model_type == 'MLM':
+            prediction_dict[row_count]['vs_pair'] = fill_verb_target(model_name, seq, verb_list, tokenizer, lang_model)
 
         end = timer()
         row_count += 1
 
-        print('%d/%d' %(row_count, chunk_size)) # print('%d/%d' %(row_count, len(df)*len(_models_clm+_models_mlm)))
+        print('%d/%d' %(row_count, chunk_size))
         print('elapsed time for one iteration: %d' %(end-start))
         total_time += end-start
         print('total elapsed time: %d' %(total_time))
@@ -317,15 +403,50 @@ def ellipsis(df):
             if not os.path.exists(_output_folder):
                 os.makedirs(_output_folder)
             pd.DataFrame(prediction_dict).T.to_csv(_output_folder + 'ellipsis-evaluation-%s_row%d.csv' %(model_name, row_count), index=False)
+    # return prediction_dict
 
+'''
+--------------------------------------------------------------------
+    Run Tasks
+--------------------------------------------------------------------
+'''
 
-if task == 'header_selection':
-    header_selection(df, model_type)
-elif task == 'rejection':
-    rejection(df)
-elif task == 'conjunction':
-    conjunction(df)
-elif task == 'ellipsis':
-    ellipsis(df)
+# run by chunk
+_start_ind = 0
+input_size = 600 # maximum input_size is 600 (= 6P2 verb pair combinations * 10 items * 2 headers)
+chunk_size = input_size - _start_ind # needs to be a multiple of 100
+
+# --- assign model
+model_name= 'distilgpt2' # an example # _supported_models[5]
+
+if model_name not in _supported_models:
+    raise ValueError("Unsupported model: {}".format(model_name))
+elif model_name == 'distilgpt2':
+    model_type = 'CLM'
 else:
-    print("unsupported task {}".format(task))
+    model_type = 'MLM'
+print("** Model: {} / Model type: {}".format(model_name, model_type))
+
+# --- assign task
+task = 'header_selection' # an example # _supported_tasks[0]
+
+if task not in _supported_tasks:
+    raise ValueError("Unsupported task: {}".format(task))
+
+elif task == 'header_selection':
+    print("** Task: {}".format(task))
+    header_selection(df_input, model_type)
+
+elif task == 'rejection':
+    print("** Task: {}".format(task))
+    rejection(df_input, model_type)
+
+elif task == 'conjunction':
+    print("** Task: {}".format(task))
+    conjunction(df_input, model_type)
+
+elif task == 'ellipsis':
+    print("** Task: {}".format(task))
+    ellipsis(df_input, model_type)
+
+print("** -- evaluate.py completed -- **")
